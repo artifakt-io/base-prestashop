1e5262e103e5f4c1ad6d4090c6bf19ee81772970
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2023-03-21 09:18:11 +0100

    Prestashop init

--- .artifakt/init-host.sh
+++ .artifakt/init-host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [ "$DEBUG" = "true" ] && set -x
 
-cp -f /var/lib/docker/volumes/services_nginxfpm-conf/_data/default.conf /etc/nginx/conf.d/default.conf  || echo "No config default.conf"
+# cp -f /var/lib/docker/volumes/services_nginxfpm-conf/_data/default.conf /etc/nginx/conf.d/default.conf  || echo "No config default.conf"
