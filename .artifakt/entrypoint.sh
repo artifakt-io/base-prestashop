7555f01ffba7c5b537298bd6e5ec8615885beeda
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2023-03-29 14:44:59 +0200

    fix again 7

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -131,8 +131,16 @@ fi
 if [ -d "$APP_ROOT_PATH/admin" ]; then
     echo "###############################################################"
     echo "##### ADMIN FOLDER NAME CHANGE: admin to $PRESTASHOP_ADMIN_PATH"
-        # cd $APP_ROOT_PATH || exit
-        mv admin "$PRESTASHOP_ADMIN_PATH"
+        cd $APP_ROOT_PATH || exit
+        mv admin back_admin
+        rm -rf admin*
+        mv back_admin "$PRESTASHOP_ADMIN_PATH"
+    else
+            cd $APP_ROOT_PATH || exit
+
+        mv "$PRESTASHOP_ADMIN_PATH" back_admin
+        rm -rf admin*
+        mv back_admin "$PRESTASHOP_ADMIN_PATH"
 fi
  
 echo "###############################################################"
